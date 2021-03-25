bc760cdca5aa289db7cfb466964ed91e0b42a632
Isaac Hulse <isaac.hulse@anaplan.com>
2021-03-25 08:53:35 +0000

    try create-react-app

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -20,3 +20,8 @@ cfg_if! {
 pub fn greet() -> String {
     "Hello, plant-based-er".to_string()
 }
+
+pub fn readCsv() {
+    let url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSmV-ya43VsyRE4ssuaQEFBjIzHDzNqLKkEeY3nMevKvNXNoc_BS0ZW2aa6POGT7Y3XCX8Z64QOLPeA/pub?output=csv";
+    
+}
